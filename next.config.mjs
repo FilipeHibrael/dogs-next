90607d32ef993faa23d36d71de97ca63f411d017
FilipeHibrael <filipehibrael27@gmail.com>
2024-05-28 22:38:46 -0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dogsapi.origamid.dev',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
