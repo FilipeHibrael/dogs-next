38f3d7a75f0e2f8c327eb36bef81bc33f07cf531
FilipeHibrael <filipehibrael27@gmail.com>
2024-05-30 21:30:08 -0300

    useForm

---
+++ src/functions/api-error.ts
@@ -0,0 +1,9 @@
+export default function apiError(error: unknown): {
+  data: null;
+  ok: false;
+  error: string;
+} {
+  if (error instanceof Error) {
+    return { ok: false, data: null, error: error.message };
+  } else return { ok: false, data: null, error: 'Erro genérico' };
+}
