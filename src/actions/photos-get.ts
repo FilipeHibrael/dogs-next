b37b1066f6ee3212c76653ff687570bf78335cba
FilipeHibrael <filipehibrael27@gmail.com>
2024-06-04 16:49:50 -0300

    scroll infinito

--- src/actions/photos-get.ts
+++ src/actions/photos-get.ts
@@ -21,18 +21,18 @@ type PhotosGetParams = {
   user?: 0 | string;
 };
 
-export default async function photosGet({
-  page = 1,
-  total = 6,
-  user = 0,
-}: PhotosGetParams = {}) {
+export default async function photosGet(
+  { page = 1, total = 6, user = 0 }: PhotosGetParams = {},
+  optionsFront?: RequestInit
+) {
   try {
-    const { url } = PHOTOS_GET({ page, total, user });
-    const respose = await fetch(url, {
+    const options = optionsFront || {
       next: { revalidate: 10, tags: ['photos'] },
-    });
+    };
+    const { url } = PHOTOS_GET({ page, total, user });
+    const respose = await fetch(url, options);
     if (!respose.ok) throw new Error('Error ao pegar as fotos');
-    
+
     const data = (await respose.json()) as Photo[];
     return { data, ok: true, error: '' };
   } catch (error) {
