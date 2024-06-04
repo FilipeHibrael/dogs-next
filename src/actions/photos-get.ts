81a4c8c5a10c7162bb94095eccd1ce3ee7cf02ee
FilipeHibrael <filipehibrael27@gmail.com>
2024-06-04 16:07:26 -0300

    feed conta

--- src/actions/photos-get.ts
+++ src/actions/photos-get.ts
@@ -1,5 +1,8 @@
 'use server';
 
+import { PHOTOS_GET } from '@/functions/api';
+import apiError from '@/functions/api-error';
+
 export type Photo = {
   id: number;
   author: string;
@@ -12,11 +15,27 @@ export type Photo = {
   total_comments: string;
 };
 
-export default async function photosGet() {
-  const respose = await fetch(
-    'https://dogsapi.origamid.dev/json/api/photo/?_page=1&_total=6&_user=0',
-    { next: { revalidate: 10, tags: ['photos'] } }
-  );
-  const data = (await respose.json()) as Photo[];
-  return data;
+type PhotosGetParams = {
+  page?: number;
+  total?: number;
+  user?: 0 | string;
+};
+
+export default async function photosGet({
+  page = 1,
+  total = 6,
+  user = 0,
+}: PhotosGetParams = {}) {
+  try {
+    const { url } = PHOTOS_GET({ page, total, user });
+    const respose = await fetch(url, {
+      next: { revalidate: 10, tags: ['photos'] },
+    });
+    if (!respose.ok) throw new Error('Error ao pegar as fotos');
+    
+    const data = (await respose.json()) as Photo[];
+    return { data, ok: true, error: '' };
+  } catch (error) {
+    return apiError(error);
+  }
 }
--- src/functions/api.ts
+++ src/functions/api.ts
@@ -28,13 +28,17 @@ export function PHOTO_POST() {
   return { url: API_URL + '/api/photo' };
 }
 
-export function PHOTOS_GET({ page, total, user }) {
+export function PHOTOS_GET({
+  page,
+  total,
+  user,
+}: {
+  page: number;
+  total: number;
+  user: 0 | string;
+}) {
   return {
     url: `${API_URL}/api/photo/?_page=${page}&_total=${total}&_user=${user}`,
-    options: {
-      method: 'GET',
-      cache: 'no-store',
-    },
   };
 }
 
